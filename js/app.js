047a1df3f7847580ce78bf878a36d43ab2186dff
ShajidShahriar <shajidsr12@gmail.com>
2025-07-22 21:00:05 +0600

    initialize calorietracker class , meal class and workout class.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,47 @@
 class Calorietracker{
+constructor(){
+this._calorieLimit = 2000
+this._totalCalories = 0
+this._meals = []
+this._workouts = []
+
+}
+
+addMeal(meal){
+    this._meals.push(meal)
+    this._totalCalories += meal.calories
+}
+addWokrout(workout){
+    this._workouts.push(workout)
+    this._totalCalories -= workout.calories
+}
 
 }
 
 class Meal{
-
+    constructor(name,calories){
+        this.id = Math.random().toString(16).slice(2)
+        this.name = name
+        this.calories = calories
+    }
 }
 class Workout{
-
+    constructor(name,calories){
+        this.id = Math.random().toString(16).slice(2)
+        this.name = name
+        this.calories = calories
+    }
 }
+
+const tracker = new Calorietracker()
+
+const breakfast = new Meal('morning-meal',200)
+tracker.addMeal(breakfast)
+const workout = new Workout('running',400)
+tracker.addWokrout(workout)
+
+
+console.log(breakfast)
+console.log(workout)
+console.log(tracker)
+
